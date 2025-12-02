131949d4d5c6840c4120395eebe7c5a950e76302
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-01 20:01:47 -0500

    Cleanup day 1

--- day01/main.ml
+++ day01/main.ml
@@ -1,23 +1,11 @@
 open! Core
 open! Common
 
-type direction =
-  | LEFT
-  | RIGHT
-
-type move =
-  { direction : direction
-  ; amount : int
-  }
+let num_positions = 100
 
 let part1 puzzle =
-  List.fold puzzle ~init:(50, 0) ~f:(fun (position, num_zeros) move ->
-    let position =
-      let num_positions = 100 in
-      match move.direction with
-      | LEFT -> (position + (num_positions - move.amount)) % num_positions
-      | RIGHT -> (position + move.amount) % num_positions
-    in
+  List.fold puzzle ~init:(50, 0) ~f:(fun (position, num_zeros) amount ->
+    let position = (position + amount) % num_positions in
     let num_zeros = if position = 0 then num_zeros + 1 else num_zeros in
     position, num_zeros)
   |> snd
@@ -25,20 +13,19 @@ let part1 puzzle =
 ;;
 
 let part2 puzzle =
-  List.concat_map puzzle ~f:(fun move ->
-    List.init move.amount ~f:(fun _ -> { move with amount = 1 }))
+  List.concat_map puzzle ~f:(fun amount ->
+    List.init (abs amount) ~f:(fun _ -> amount / abs amount))
   |> part1
 ;;
 
 let parse =
   let open Angstrom in
   let open Angstrom.Let_syntax in
-  let move =
-    let%bind direction = char 'L' *> return LEFT <|> char 'R' *> return RIGHT in
-    let%map amount = integer in
-    { direction; amount }
+  let amount =
+    let%bind direction = char 'L' *> return Int.neg <|> char 'R' *> return Fn.id in
+    integer >>| direction
   in
-  many_lines_of move |> exec_exn
+  many_lines_of amount |> exec_exn
 ;;
 
 let () = run_with_input_file ~part1 ~part2 ~parse
