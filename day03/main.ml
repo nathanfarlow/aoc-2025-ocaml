158bfa9c666765a77f72cb8b65ee1859ef62059c
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-04 00:01:20 -0500

    Cleanup

--- day03/main.ml
+++ day03/main.ml
@@ -5,13 +5,13 @@ let rec choose k xs =
   match k with
   | 0 -> 0
   | _ ->
-    let len = List.length xs in
-    let available = List.take xs (len - k + 1) in
-    (match List.max_elt ~compare:Int.compare available with
-     | Some max ->
-       let remaining = List.drop_while xs ~f:(( <> ) max) in
-       (Int.pow 10 (k - 1) * max) + choose (k - 1) (List.tl_exn remaining)
-     | None -> 0)
+    let max =
+      List.take xs (List.length xs - k + 1)
+      |> List.max_elt ~compare:Int.compare
+      |> Option.value_exn
+    in
+    let remaining = List.drop_while xs ~f:(( <> ) max) |> List.tl_exn in
+    (Int.pow 10 (k - 1) * max) + choose (k - 1) remaining
 ;;
 
 let part k = sum ~f:(choose k) >> print_int
