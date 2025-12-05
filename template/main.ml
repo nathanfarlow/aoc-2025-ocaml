4e697a5dede367c415c24e98b2e0d8a3fa25d241
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-04 23:50:50 -0500

    Fix

--- template/main.ml
+++ template/main.ml
@@ -6,8 +6,8 @@ let part2 _ = failwith ""
 
 let parse =
   let open Angstrom in
-  let _ = return () in
-  many_lines_of (many _) |> exec_exn
+  let __ = return () in
+  many_lines_of (many __) |> exec_exn
 ;;
 
 let () = run_with_input_file ~part1 ~part2 ~parse
