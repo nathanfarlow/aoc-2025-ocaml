513b760ab8ef4a503d2594a6725fc76a29660266
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-06 22:34:41 -0500

    Cleanup part 2

--- day06/main.ml
+++ day06/main.ml
@@ -1,6 +1,13 @@
 open! Core
 open! Common
 
+let part1 (rows, ops) =
+  List.transpose_exn rows
+  |> List.zip_exn ops
+  |> sum ~f:(fun (f, ints) -> List.reduce_exn ints ~f)
+  |> print_int
+;;
+
 let op =
   let open Angstrom in
   char '*' *> return ( * ) <|> char '+' *> return ( + )
@@ -12,28 +19,19 @@ let parse_part1 =
   both (many_lines_of ints) (sep_by space op) |> exec_exn
 ;;
 
-let part1 (rows, ops) =
-  List.transpose_exn rows
-  |> List.zip_exn ops
-  |> sum ~f:(fun (f, ints) -> List.reduce_exn ints ~f)
-  |> print_int
-;;
-
-let part1 = parse_part1 >> part1
-
 let parse_part2 =
   let open Angstrom in
-  let int_line =
+  let ints =
     many1 (satisfy Char.is_digit <|> char ' ' >>| Char.to_string >>| String.strip)
   in
   let ops =
-    let%bind op = char '*' *> return `Mul <|> char '+' *> return `Add in
+    let%bind op = op in
     let%bind count = many (char ' ') >>| List.length in
     return (op, count + 1)
   in
   let p =
-    let%bind ints = many_lines_of ~trim:false int_line in
-    let%bind ops = many ops <* space_or_line in
+    let%bind ints = many_lines_of ~trim:false ints in
+    let%bind ops = many ops <* space_or_line >>| List.unzip in
     return (ints, ops)
   in
   p |> exec_exn ~trim:false
@@ -47,9 +45,7 @@ let rec split nums xs =
     fst :: split rest snd
 ;;
 
-let part2 ((p : string list list), (k : ([> `Add | `Mul ] * int) list)) =
-  let _ops = List.map k ~f:fst in
-  let nums = List.map k ~f:snd in
+let part2 ((p : string list list), (_ops, nums)) =
   let rows = List.map p ~f:(split nums) in
   let _cols = List.transpose_exn rows in
   let _hm = List.map _cols ~f:List.transpose_exn in
@@ -57,13 +53,8 @@ let part2 ((p : string list list), (k : ([> `Add | `Mul ] * int) list)) =
     List.map _hm ~f:(fun column ->
       List.filter_map column ~f:(fun node -> String.concat node |> Int.of_string_opt))
   in
-  List.zip_exn _ops _int
-  |> sum ~f:(fun (op, ints) ->
-    match op with
-    | `Mul -> List.reduce_exn ints ~f:( * )
-    | `Add -> List.reduce_exn ints ~f:( + ))
-  |> print_int
+  List.zip_exn _ops _int |> sum ~f:(fun (f, ints) -> List.reduce_exn ints ~f) |> print_int
 ;;
 
 let part2 = parse_part2 >> part2
-let () = run_with_input_file ~part1 ~part2 ~parse:Fn.id
+let () = run_with_input_file ~part1:(parse_part1 >> part1) ~part2 ~parse:Fn.id
