4f5e8dcf3ec6d7495d26eb646c69d341af2a1d49
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-07 01:14:20 -0500

    .

--- day07/main.ml
+++ day07/main.ml
@@ -1,26 +1,18 @@
 open! Core
 open! Common
 
-[@@@warning "-37"]
-
-type cell =
-  | Empty
-  | Start
-  | Split
-  | Beam
-
 let calculate grid =
   Memo.recursive ~hashable:Point.hashable (fun calculate (i, j) ->
     let c = ref 0 in
     let rec loop i =
       match Grid.get_opt grid (i, j) with
-      | Some Start -> incr c
-      | None | Some Split -> ()
+      | Some 'S' -> incr c
+      | None | Some '^' -> ()
       | _ ->
         [ i, j - 1; i, j + 1 ]
         |> List.iter ~f:(fun pos ->
           match Grid.get_opt grid pos with
-          | Some Split -> c := !c + calculate pos
+          | Some '^' -> c := !c + calculate pos
           | _ -> ());
         loop (i - 1)
     in
@@ -31,7 +23,7 @@ let calculate grid =
 let part2 grid =
   let calculate = calculate grid in
   Grid.find_all grid ~f:(fun _ -> function
-    | Split -> true
+    | '^' -> true
     | _ -> false)
   |> sum ~f:calculate
   |> ( + ) 1
@@ -41,7 +33,7 @@ let part2 grid =
 let part1 grid =
   let calculate = calculate grid in
   Grid.find_all grid ~f:(fun _ -> function
-    | Split -> true
+    | '^' -> true
     | _ -> false)
   |> List.map ~f:calculate
   |> List.count ~f:(( < ) 0)
@@ -50,12 +42,7 @@ let part1 grid =
 
 let parse =
   let open Angstrom in
-  let line =
-    let one =
-      char '.' *> return Empty <|> char 'S' *> return Start <|> char '^' *> return Split
-    in
-    many one
-  in
+  let line = many (not_char '\n') in
   many_lines_of line >>| Grid.create |> exec_exn
 ;;
 
