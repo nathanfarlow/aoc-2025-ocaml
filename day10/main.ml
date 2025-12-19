991678701216f429fd38d54e528a2a42feb00860
Nathan Farlow <natethegreatest11@gmail.com>
2025-12-18 21:51:19 -0500

    Day 10 part 1

---
+++ day10/main.ml
@@ -0,0 +1,50 @@
+open! Core
+open! Common
+
+module M = struct
+  type t = bool list [@@deriving hash, compare, sexp]
+end
+
+let toggle state button =
+  let state = Array.of_list state in
+  List.iter button ~f:(fun i -> state.(i) <- not state.(i));
+  Array.to_list state
+;;
+
+let fewest_presses (target, _buttons, _joltages) =
+  let seen = Hash_set.create (module M) in
+  let queue = Queue.create () in
+  Queue.enqueue queue (0, List.init (List.length target) ~f:(fun _ -> false));
+  let rec go () =
+    let depth, value = Queue.dequeue_exn queue in
+    if List.equal Bool.equal value target
+    then depth
+    else (
+      List.iter _buttons ~f:(fun button ->
+        let toggled = toggle value button in
+        if not (Hash_set.mem seen toggled) then Queue.enqueue queue (depth + 1, toggled));
+      go ())
+  in
+  go ()
+;;
+
+let part1 = sum ~f:fewest_presses >> print_int
+let part2 _ = failwith ""
+
+let parse =
+  let open Angstrom in
+  let ints = sep_by1 (char ',') integer in
+  let target =
+    let light = char '.' *> return false <|> char '#' *> return true in
+    char '[' *> many1 light <* char ']'
+  in
+  let buttons =
+    let button = char '(' *> ints <* char ')' in
+    sep_by1 (char ' ') button
+  in
+  let joltages = char '{' *> ints <* char '}' in
+  let machine = lift3 Tuple3.create (target <* space) (buttons <* space) joltages in
+  many_lines_of machine |> exec_exn
+;;
+
+let () = run_with_input_file ~part1 ~part2 ~parse
