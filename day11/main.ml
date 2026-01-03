32f7107b8390beb856f00287eeb6d61951c208cf
Nathan Farlow <natethegreatest11@gmail.com>
2026-01-03 13:19:42 -0500

    Clean up day 11

--- day11/main.ml
+++ day11/main.ml
@@ -1,16 +1,14 @@
 open! Core
 open! Common
 
-let part1 (data : (string * string list) list) =
-  let dag = Hashtbl.create (module String) in
-  List.iter data ~f:(fun (node, edges) -> Hashtbl.set dag ~key:node ~data:edges);
-  let num =
-    Memo.recursive ~hashable:String.hashable (fun num node ->
+let part1 dag =
+  let num_paths =
+    Memo.recursive ~hashable:String.hashable (fun num_paths node ->
       match node with
       | "out" -> 1
-      | _ -> Hashtbl.find dag node |> Option.value ~default:[] |> sum ~f:num)
+      | _ -> Map.find dag node |> Option.value ~default:[] |> sum ~f:num_paths)
   in
-  print_int (num "you")
+  print_int (num_paths "you")
 ;;
 
 module Key = struct
@@ -27,22 +25,21 @@ module Key = struct
   include Hashable.Make (T)
 end
 
-let part2 (data : (string * string list) list) =
-  let dag = Hashtbl.create (module String) in
-  List.iter data ~f:(fun (node, edges) -> Hashtbl.set dag ~key:node ~data:edges);
-  let num =
-    Memo.recursive ~hashable:Key.hashable (fun num key ->
+let part2 dag =
+  let num_paths =
+    Memo.recursive ~hashable:Key.hashable (fun num_paths key ->
       match key.node with
       | "out" -> if key.fft && key.dac then 1 else 0
       | _ ->
-        let fft = key.fft || String.equal key.node "fft" in
-        let dac = key.dac || String.equal key.node "dac" in
-        Hashtbl.find dag key.node
+        Map.find dag key.node
         |> Option.value ~default:[]
-        |> List.map ~f:(fun node -> { Key.fft; dac; node })
-        |> sum ~f:num)
+        |> List.map ~f:(fun node ->
+          let fft = key.fft || String.equal key.node "fft" in
+          let dac = key.dac || String.equal key.node "dac" in
+          { Key.fft; dac; node })
+        |> sum ~f:num_paths)
   in
-  print_int (num { node = "svr"; fft = false; dac = false })
+  print_int (num_paths { node = "svr"; fft = false; dac = false })
 ;;
 
 let parse =
@@ -53,7 +50,7 @@ let parse =
       (take_till_p (string ": "))
       (sep_by1 (char ' ') (take_while1 (Fn.non Char.is_whitespace)))
   in
-  many_lines_of line |> exec_exn
+  many_lines_of line >>| Map.of_alist_exn (module String) |> exec_exn
 ;;
 
 let () = run_with_input_file ~part1 ~part2 ~parse
